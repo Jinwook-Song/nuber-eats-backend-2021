5fe8823abb38444e4790af2059d99995c4ec3df5
jinwook-song <wlsdnr129@naver.com>
2021-05-10 19:05:06 +0900

    lecture 6.8 mailgun templates

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,15 +10,17 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
     // nestJS가 실행 될때마다 test
-    this.sendEmail('testing', 'test');
+    // this.sendEmail('testing', 'test');
   }
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(subject: string, template: string) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `wlsdnr129@naver.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:code', 'this');
+    form.append('v:username', 'Jinwook');
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
