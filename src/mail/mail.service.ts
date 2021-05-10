54598fd93c42934317eba028fed1f188c6b0f495
jinwook-song <wlsdnr129@naver.com>
2021-05-10 18:25:16 +0900

    lecture 6.7 Mailgun API

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    // nestJS가 실행 될때마다 test
+    this.sendEmail('testing', 'test');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `wlsdnr129@naver.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        method: 'POST',
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
