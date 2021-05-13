9222fe5e80434820b196862b5ebc13dfc6647e38
jinwook-song <wlsdnr129@naver.com>
2021-05-13 20:15:47 +0900

    lecture 8.4 sendEmail Test

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -15,7 +15,7 @@ export class MailService {
     // to: string,
     template: string,
     emailVars: EmailVars[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append(
       'from',
@@ -27,17 +27,20 @@ export class MailService {
     emailVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
 
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+      await got.post(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        method: 'POST',
-        body: form,
-      });
+      );
+      return true;
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
