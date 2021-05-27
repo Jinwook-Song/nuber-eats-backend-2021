2195bb98a51bed21c73ea38f8ed0c1f698013191
jinwook-song <wlsdnr129@naver.com>
2021-05-27 20:47:50 +0900

    lecture 11.12 Create Order part 6

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -25,54 +25,72 @@ export class OrderService {
     customer: User,
     { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne(restaurantId);
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'Restaurant not found',
-      };
-    }
-    // forEach doesn't work when error erupt
-    for (const item of items) {
-      const dish = await this.dishes.findOne(item.dishId);
-      if (!dish) {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
         return {
           ok: false,
-          error: 'Dish not found',
+          error: 'Restaurant not found',
         };
       }
-      for (const itemOption of item.options) {
-        const dishOption = dish.options.find(
-          (dishOption) => dishOption.name === itemOption.name,
-        );
-        if (dishOption) {
-          if (dishOption.extra) {
-            console.log(`$USD + ${dishOption.extra}`);
-          } else {
-            const dishOptionChoice = dishOption.choices.find(
-              (optionChoice) => optionChoice.name === itemOption.choice,
-            );
-            if (dishOptionChoice) {
-              if (dishOptionChoice.extra) {
-                console.log(`$USD + ${dishOptionChoice.extra}`);
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      // forEach doesn't work when error erupt
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          return {
+            ok: false,
+            error: 'Dish not found',
+          };
+        }
+        let dishFinalPrice = dish.price;
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+          if (dishOption) {
+            if (dishOption.extra) {
+              dishFinalPrice += dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choices.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  dishFinalPrice += dishOptionChoice.extra;
+                }
               }
             }
           }
         }
+        orderFinalPrice += dishFinalPrice;
+
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
       }
-      // await this.orderItems.save(
-      //   this.orderItems.create({
-      //     dish,
-      //     options: item.options,
-      //   }),
-      // );
+
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          items: orderItems,
+          total: orderFinalPrice,
+        }),
+      );
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'Could not create order',
+      };
     }
-    // const order = await this.orders.save(
-    //   this.orders.create({
-    //     customer,
-    //     restaurant,
-    //   }),
-    // );
-    // console.log(order);
   }
 }
