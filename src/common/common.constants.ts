d06e09cd019be3d6fde1da9789b3427d5a1c6108
jinwook-song <wlsdnr129@naver.com>
2021-05-28 18:57:40 +0900

    lecture 12.14 PUB_SUB

--- src/common/common.constants.ts
+++ src/common/common.constants.ts
@@ -1,1 +1,2 @@
 export const CONFIG_OPTIONS = 'CONFIG_OPTIONS';
+export const PUP_SUB = 'PUP_SUB';
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { PUP_SUB } from './common.constants';
 
-@Module({})
+const pubsub = new PubSub();
+
+@Global()
+@Module({
+  providers: [{ provide: PUP_SUB, useValue: pubsub }],
+  exports: [PUP_SUB],
+})
 export class CommonModule {}
