06426a46f2eca01eae74d42eb80d3a1fd9fa3e52
jinwook-song <wlsdnr129@naver.com>
2021-06-04 18:04:50 +0900

    lecture 20.3 Uploads to AWS

---
+++ src/uploads/uploads.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Injectable,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = 'kimchinubereats7926';
+
+@Injectable()
+@Controller('uploads')
+export class UploadsController {
+  constructor(private readonly config: ConfigService) {}
+
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: this.config.get('AWS_ACCESSKEYID'),
+        secretAccessKey: this.config.get('AWS_SECRETACCESSKEY'),
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (error) {
+      return error;
+    }
+  }
+}
